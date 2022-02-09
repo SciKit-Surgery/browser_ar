66badd5f006f97ef34daca2827c590b799047bdc
Stephen Thompson <s.thompson@ucl.ac.uk>
2022-02-09 16:05:40 +0000

    Issue #3 added constraints to video grabber

--- src/video.js
+++ src/video.js
@@ -4,12 +4,20 @@
 
 const video = document.getElementById('videoElement');
 
+var constraints = {
+    audio: false,
+    video: {
+        width: { min: 1024, ideal: 1280, max: 1920 },
+        height: { min: 576, ideal: 720, max: 1080 },
+    }
+};
+
 if (navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia({ video: true })
+  navigator.mediaDevices.getUserMedia(constraints)
     .then(function (stream) {
       video.srcObject = stream;
     })
     .catch(function (err0r) {
-      console.log("Something went wrong!");
+      console.log("Video Capture from Webcam failed.");
     });
 }
